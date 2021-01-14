8be788582ff294e970304a9206f249591189ae56
Harun Or Rashid <horrashid007@gmail.com>
2021-01-14 20:24:45 +0600

    configure sharp for image processing

--- multer.js
+++ multer.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const sharp = require('sharp');
 
 // Store image in memory not file
 const multerStorage = multer.memoryStorage();
@@ -17,3 +18,26 @@ const upload = multer({
 });
 
 exports.uploadSingleImage = upload.single('photo');
+
+exports.resizeUploadedImage = async (req, res, next) => {
+  try {
+    if (!req.file) return next();
+
+    const fileBuffer = await sharp(req.file.buffer)
+      .resize(200, 200)
+      .toFormat('jpeg')
+      .jpeg({ quality: 90 })
+      .toBuffer();
+
+    req.file.buffer = fileBuffer;
+
+    next();
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Image upload failed! Please try again',
+    });
+  }
+
+  next();
+};
